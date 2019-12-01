9721e8408b530e281e96593eabaecdf52b571372
Teun Duynstee <teun@duynstee.com>
2019-12-01 14:48:11 +0100

    Fix

--- src/2019/1.ts
+++ src/2019/1.ts
@@ -1,15 +1,19 @@
-import * as rig from "./modules/rig";
+import {Rig} from "./modules/rig";
 import { parseToObjects } from "./modules/lineParser";
 
 const massToFuel = (mass: number) => {
     return Math.floor(mass / 3) - 2;
 }
-rig.read("1").then(
+const rig = new Rig(1, 
     async (d) => {
         const values = parseToObjects(d, /.*/, (s, n) => {
             return Number(s[0]);
         });
         const result = values.reduce((a,v)=>a+massToFuel(v), 0);
-        console.log(result);
+        return result;
     }
-)
+);
+(async () => {
+    await rig.testFromFile("1", 33583);
+    await rig.runPrint();
+})().then(()=>{console.log("Done");});
