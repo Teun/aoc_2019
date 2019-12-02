55feb4db9635e73420faaeb2b6ae43727ad75404
Teun Duynstee <teun@duynstee.com>
2019-12-02 18:34:32 +0100

    Stronger typing of next step

--- src/2019/modules/IntCodeMachine.ts
+++ src/2019/modules/IntCodeMachine.ts
@@ -1,3 +1,5 @@
+type DoNext = "Exit" | "Next";
+
 export class IntCodeMachine {
     constructor(private memory: number[]) {
         
@@ -6,32 +8,44 @@ export class IntCodeMachine {
     public get Memory() : number[] {
         return this.memory;
     }
-    /**
-     * Run
-     */
+
     public Run() {
         while(this.Step()){
             // do nothing
         }
         
     }
+
+
+
     private Step(): boolean {
         const result = this.ExecOp();
-        if(result) { this._pointer += 4;}
-        return result;
+        if(result === "Next") { this._pointer += 4;}
+        return result !== "Exit";
     }
-    private ExecOp() {
+    
+    public get arg1() : number {
+        return this.Memory[this._pointer + 1];
+    }
+    public get arg2() : number {
+        return this.Memory[this._pointer + 2];
+    }
+    public get arg3() : number {
+        return this.Memory[this._pointer + 3];
+    }
+    
+    private ExecOp(): DoNext {
         switch (this.memory[this._pointer]) {
             case 1:
-                const sum = this.memory[this.memory[this._pointer + 1]] + this.memory[this.memory[this._pointer + 2]];
-                this.memory[this.memory[this._pointer + 3]] = sum;
-                return true;
+                const sum = this.memory[this.arg1] + this.memory[this.arg2];
+                this.memory[this.arg3] = sum;
+                return "Next";
             case 2:
-                const prod = this.memory[this.memory[this._pointer + 1]] * this.memory[this.memory[this._pointer + 2]];
-                this.memory[this.memory[this._pointer + 3]] = prod;
-                return true;
+                const prod = this.memory[this.arg1] * this.memory[this.arg2];
+                this.memory[this.arg3] = prod;
+                return "Next";
             case 99: 
-                return false;
+                return "Exit";
             default:
                 throw "Unexpected op";
         }
