07ffbb6bca8a9b862b922a6848457e5eade8dea9
Teun Duynstee <teun@duynstee.com>
2019-12-06 10:32:11 +0100

    Refactoring on IntCodeMachine

--- src/2019/modules/IntCodeMachine.ts
+++ src/2019/modules/IntCodeMachine.ts
@@ -1,20 +1,22 @@
-type DoNext = "Exit" | "Next4" | "Next3" | "Next2" | "Stay";
+type DoNext = "Exit" | "Stay" | number;
 
 interface Op {
     baseOp: number;
-    argMode1: number;
-    argMode2: number;
-    argMode3: number;
+    argMode1: ArgMode;
+    argMode2: ArgMode;
+    argMode3: ArgMode;
 }
+type ArgMode = 0 | 1;
+
 const OpFromNum = (num: number): Op => {
+    const bitOnPos = (full: number, pos: number): ArgMode => {
+        const padded = "00000" + full;
+        return padded[padded.length - pos] === "0" ? 0 : 1;
+    }
     const baseOp = num % 100;
-    num = (num - baseOp) / 100;
-    const argMode1 = num % 10;
-    num = (num - argMode1) / 10;
-    const argMode2 = num % 10;
-    num = (num - argMode2) / 10;
-    const argMode3 = num % 10;
-    num = (num - argMode3) / 10;
+    const argMode1: ArgMode = bitOnPos(num, 3);
+    const argMode2: ArgMode = bitOnPos(num, 4);
+    const argMode3: ArgMode = bitOnPos(num, 5);
     return {
         baseOp,
         argMode1,
@@ -27,14 +29,16 @@ export class IntCodeMachine {
     public get Memory(): number[] {
         return this.memory;
     }
-    public get arg1(): number {
-        return this.Memory[this._pointer + 1];
+    private get(arg: number, argMode: number) {
+        if(argMode === 0){
+            return this.Memory[arg];
+        }else{
+            return arg;
+        }
     }
-    public get arg2(): number {
-        return this.Memory[this._pointer + 2];
-    }
-    public get arg3(): number {
-        return this.Memory[this._pointer + 3];
+    private arg(nr: number, op?: Op) {
+        const argMode = op ? op["argMode" + nr] : 1;
+        return this.get(this.memory[nr + this._pointer], argMode);
     }
     private _pointer: number = 0;
     private _stdin: number[] = [];
@@ -56,13 +60,6 @@ export class IntCodeMachine {
 
     constructor(private memory: number[]) {
     }
-    private get(arg: number, argMode: number) {
-        if(argMode === 0){
-            return this.Memory[arg];
-        }else{
-            return arg;
-        }
-    }
 
     public Run() {
         while (this.Step()) {
@@ -72,63 +69,61 @@ export class IntCodeMachine {
 
     private Step(): boolean {
         const result = this.ExecOp();
-        if (result === "Next4") { this._pointer += 4; }
-        if (result === "Next3") { this._pointer += 3; }
-        if (result === "Next2") { this._pointer += 2; }
+        if (typeof(result) === "number") { this._pointer += result; }
         return result !== "Exit";
     }
 
     private ExecOp(): DoNext {
         const operation = OpFromNum(this.memory[this._pointer]);
         switch (operation.baseOp) {
-            case 1:
-                const sum = this.get(this.arg1, operation.argMode1) + 
-                    this.get(this.arg2, operation.argMode2);
-                this.memory[this.arg3] = sum;
-                return "Next4";
-            case 2:
-                const prod = this.get(this.arg1, operation.argMode1) * 
-                    this.get(this.arg2, operation.argMode2);
-                this.memory[this.arg3] = prod;
-                return "Next4";
-            case 3:
+            case 1: // add
+                const sum = this.arg(1, operation) + 
+                    this.arg(2, operation);
+                this.memory[this.arg(3)] = sum;
+                return 4;
+            case 2: // mult
+                const prod = this.arg(1, operation) * 
+                    this.arg(2, operation);
+                this.memory[this.arg(3)] = prod;
+                return 4;
+            case 3: // read
                 const rd = this.readIn();
-                this.memory[this.arg1] = rd;
-                return "Next2";
-            case 4:
-                const wr = this.get(this.arg1, operation.argMode1);
+                this.memory[this.arg(1)] = rd;
+                return 2;
+            case 4: // write
+                const wr = this.arg(1, operation);
                 this.writeOut(wr);
-                return "Next2";
-            case 5:
-                const to_nonzero = this.get(this.arg2, operation.argMode2);
-                if(this.get(this.arg1, operation.argMode1) !== 0){
+                return 2;
+            case 5: // jmp_nonzero
+                const to_nonzero = this.arg(2, operation);
+                if(this.arg(1, operation) !== 0){
                     this._pointer = to_nonzero;
                     return "Stay";
                 }
-                return "Next3"
-            case 6:
-                const to_zero = this.get(this.arg2, operation.argMode2);
-                if(this.get(this.arg1, operation.argMode1) === 0){
+                return 3
+            case 6: // jmp_zero
+                const to_zero = this.arg(2, operation);
+                if(this.arg(1, operation) === 0){
                     this._pointer = to_zero;
                     return "Stay";
                 }
-                return "Next3"
-            case 7:
-                this.memory[this.arg3] = 
-                    (this.get(this.arg1, operation.argMode1) < 
-                        this.get(this.arg2, operation.argMode2)) 
+                return 3
+            case 7: // less
+                this.memory[this.arg(3)] = 
+                    (this.arg(1, operation) < 
+                        this.arg(2, operation)) 
                         ? 1 : 0;
-                return "Next4"
-            case 8:
-                    this.memory[this.arg3] = 
-                    (this.get(this.arg1, operation.argMode1) === 
-                        this.get(this.arg2, operation.argMode2)) 
+                return 4
+            case 8: // equals
+                    this.memory[this.arg(3)] = 
+                    (this.arg(1, operation) === 
+                        this.arg(2, operation))
                         ? 1 : 0;
-                return "Next4"
-            case 99:
+                return 4
+            case 99: // exit
                 return "Exit";
             default:
-                throw new Error("Unexpected op");
+                throw new Error(`Unexpected op: ${operation.baseOp}`);
         }
 
     }
