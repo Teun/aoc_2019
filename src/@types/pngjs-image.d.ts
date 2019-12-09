be2085bb9f7e73d94e3545f920d638051ac9364b
Teun Duynstee <teun@duynstee.com>
2019-12-09 21:53:30 +0100

    edit typings

--- src/@types/pngjs-image.d.ts
+++ src/@types/pngjs-image.d.ts
@@ -1,17 +1,20 @@
-declare module "pngjs-image"{
-    function createImage(width: number, height: number): PngImage;
-    interface PngImage {
-        getWidth(): number;
-        getHeight(): number;
-        setAt(x: number, y: number, color: ColorDef);
-        writeImage(file: string, cb: (err: any) => void);
-    }
-    interface ColorDef {
-        red: number;
-        green: number;
-        blue: number;
-        alpha: number;
-    }
+// Type definitions for pngjs-image 0.11
+// Project: https://github.com/yahoo/pngjs-image
+// Definitions by: Teun Duynstee <https://github.com/Teun>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+export function createImage(width: number, height: number): PngImage;
+export function readImage(filePath: string, cb: (err: any, image: PngImage) => void): void;
+
+export interface PngImage {
+    getWidth(): number;
+    getHeight(): number;
+    setAt(x: number, y: number, color: ColorDef): void;
+    writeImage(file: string, cb: (err: any) => void): void;
 }
-
+export interface ColorDef {
+    red: number;
+    green: number;
+    blue: number;
+    alpha: number;
+}
